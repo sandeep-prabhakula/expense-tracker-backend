469df8f5a51f8a456985a46276ae48e6b53802d9
sandeep-prabhakula <sandeepprabhakula10@gmail.com>
2024-09-30 12:59:08 +0530

    fixed cron job

--- src/main/java/com/sandeepprabhakula/ExpenseTracker/job/ServiceWakingupJob.java
+++ src/main/java/com/sandeepprabhakula/ExpenseTracker/job/ServiceWakingupJob.java
@@ -1,5 +1,7 @@
 package com.sandeepprabhakula.ExpenseTracker.job;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -13,12 +15,14 @@ public class ServiceWakingupJob {
     @Autowired
     RestTemplate restTemplate;
 
+    private final Logger log = LoggerFactory.getLogger(ServiceWakingupJob.class);
+
     @Scheduled(cron = "0 */12 * * * ?")
     public void getBlog(){
         try{
             String url = "https://expensetracker-mbqurkyj.b4a.run/users/job";
             String response = restTemplate.getForObject(url, String.class);
-            System.out.println("I'm awake " + new Date());
+            log.info("I'm awake {}{}", response, new Date());
         }catch (Exception e){
             System.out.println(e.getMessage() + new Date());
         }
